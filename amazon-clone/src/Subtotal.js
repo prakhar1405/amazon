fd7c4cd0b81b16da71a6f93677e451732a4e3308
prakhar1405 <prakharagarwal1405@gmail.com>
2021-11-20 18:14:41 +0530

    Added remove from basket functionality to checkout page

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -15,7 +15,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal ({basket?.length} items): <strong>{getBasketTotal(basket)}</strong>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal_gift">
               <input type="checkbox" /> This order contains a gift
@@ -23,7 +23,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"₹"}
